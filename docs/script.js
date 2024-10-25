bf685995c765c05fe0fd0fabfe8d2c7e9bef73ac
Adailson Guimaraes <99360493+hada97@users.noreply.github.com>
2024-10-25 02:46:38 -0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -7,7 +7,7 @@ document
     const senha = document.getElementById("senha").value;
 
     try {
-      const response = await fetch("http://localhost:8080/login", {
+      const response = await fetch("https://vollmed.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
